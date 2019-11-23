09fb285e054fddc9366a9dc0f65bb1040a175716
Christopher <chris.wblack@live.de>
2019-11-23 12:10:14 +0100

    Added script for selecting active arm

---
+++ Octosimilator/Assets/Scripts/TentacleSelector.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TentacleSelector : MonoBehaviour
+{
+    public TentacleNavigation[] tentacleNavigations;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(TentacleNavigation tenNav in tentacleNavigations)
+        {
+            tenNav.enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int input = -1;
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            input = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            input = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            input = 3;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            input = 4;
+        }
+        if (input > 0)
+        {
+            foreach (TentacleNavigation tenNav in tentacleNavigations)
+            {
+                tenNav.enabled = false;
+            }
+            if (input <= tentacleNavigations.Length)
+            {
+                tentacleNavigations[input - 1].enabled = true;
+            }
+        }
+    }
+}
