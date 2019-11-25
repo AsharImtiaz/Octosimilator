84fd6ce095c51cd58ca37ee7a0455e6247091a16
Christopher <chris.wblack@live.de>
2019-11-25 17:13:32 +0100

    minor fix

--- Octosimilator/Assets/Scripts/pickandThrow.cs
+++ Octosimilator/Assets/Scripts/pickandThrow.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class PickAndThrow : MonoBehaviour
+public class pickandThrow : MonoBehaviour
 {
     // Start is called before the first frame update
     bool isPicked;
